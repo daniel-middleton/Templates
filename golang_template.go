924d8c4ee405670b4789dd462b4cea9cb296871d
Daniel Middleton <me@daniel-middleton.com>
2015-01-02 19:25:39 +0000

    Fleshing out.

--- golang_template.go
+++ golang_template.go
@@ -1,44 +1,74 @@
-/*
-  Name:		Replace
-  Author:	Daniel Middleton <me@daniel-middleton.com>
-  Description:	Replace
-  Usage:	Replace
-*/
+///////////////////////////////////////////////////////////////////////////////
+// Name:	<REPLACE_NAME>
+// Author:	Daniel Middleton <me@daniel-middleton.com>
+// Description:	<REPLACE_DESC>
+// Usage:	See ./<REPLACE_NAME> -h or --help
+///////////////////////////////////////////////////////////////////////////////
 
 package main
 
-// Import dependancies
+// Package dependancies
 import (
+	"flag"
 	"fmt"
 	"os"
 	"time"
 )
 
 // Global variables
-var progName = string("Replace")
+var progName string = "Replace"
 
 // Usage: screenOut("STATUS", "message")
 func screenOut(status string, message string) {
 	timestamp := time.Now().Format("20060102T150405")
+	fmt.Printf("[%v][%v][%v]: %v \n", progName, timestamp, status, message)
+}
 
-	if status == "" {
-		status = "INFO"
+// Usage: checkStatus(err, "Error message", "Success message")
+func checkStatus(err error, errorMsg string, successMsg string) {
+	if err == nil {
+		screenOut("SUCCESS", successMsg)
+	} else {
+		screenOut("ERROR", errorMsg+" - Exiting...")
+		screenOut("ERROR", "Error report:")
+		fmt.Println("\n", err, "\n")
+		os.Exit(1)
 	}
-
-	fmt.Printf("[%v][%v][%v]: %v \n", progName, status, timestamp, message)
 }
 
 // Usage:
+func newFunc() {
+	screenOut("INFO", "Started newFunc...")
+
+	// Do stuff
+	//	checkStatus(err,
+	//		"Error message.",
+	//		"Success message.")
+
+	screenOut("INFO", "Finished newFunc.")
+}
+
+// Usage: Auto-runs on program execution
 func init() {
+	// Define and parse command line flags
+	wordPtr := flag.String("word", "foo", "a string")
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
+	flag.Parse()
 
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *boolPtr)
 }
 
-// Usage:
+// Usage: Auto-runs after init
 func main() {
-	screenOut("", progName+" script started...")
+	fmt.Println()
+	screenOut("INFO", progName+" script started...")
 
-	// Make a start
+	// Execute functions
 
-	screenOut("", progName+" script ended.")
+	screenOut("INFO", progName+" script ended.")
+	fmt.Println()
 	os.Exit(0)
 }
